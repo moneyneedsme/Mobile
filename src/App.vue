f58fdb7f84e929c5789997eca5e70337a67399f7
moneyneedsme <1311746495@qq.com>
2019-12-29 17:20:24 +0800

    '头部底部'

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <head-nav></head-nav>
+    <foot-nav></foot-nav>
+  </div>
+</template>
+
+<script>
+import headNav from './components/headNav.vue'
+import footNav from './components/footNav.vue'
+
+export default {
+  name: 'app',
+  components: {
+    headNav,
+    footNav
+  }
+}
+</script>
+
+<style>
+*{
+  margin:0px;
+  padding:0px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/footNav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class='foorNav'>
+    <van-tabbar route>
+      <van-tabbar-item
+        replace
+        to="/home"
+        icon="home-o"
+      >
+        标签
+      </van-tabbar-item>
+      <van-tabbar-item
+        replace
+        to="/search"
+        icon="search"
+      >
+        标签
+      </van-tabbar-item>
+    </van-tabbar>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'foorNav',
+  data() {
+    return {
+    }
+  },
+	methods:{
+	}
+}
+</script>
+<style lang='less' scoped>
+	// .foorNav{
+	// }
+</style>
